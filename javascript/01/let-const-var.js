547790cdca34df5b2cd4ef098dc408451ef17753
Arnav Gupta <championswimmer@gmail.com>
2022-08-20 22:45:36 +0530

    let const var and scopes
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ javascript/01/let-const-var.js
@@ -0,0 +1,18 @@
+let a = 10;
+var b = 20;
+const c = 30;
+
+a = 20;
+console.log(a);
+
+const o = { a: 10 };
+o.b = 20;
+console.log(o);
+
+// o = { a: 10, b: 20 }; // error
+
+console.log(x)
+console.log(y)
+var x = 113;       // hoisting
+let y = 321;
+
---
+++ javascript/01/let-var-scope.js
@@ -0,0 +1,32 @@
+let x = 10;
+console.log(x);
+
+function change() {
+  let x = 20;
+  console.log(x);
+  if (true) {
+    let x = 30;
+    console.log(x);
+  }
+  console.log(x);
+}
+change();
+console.log(x);
+
+
+
+// var x = 10;
+// console.log(x);
+
+// function change() {
+//   console.log(x);
+//   var x = 20;
+//   if (true) {
+//     console.log(x);
+//     var x = 30;
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+// change();
+// console.log(x);
