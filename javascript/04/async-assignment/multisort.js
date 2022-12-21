126577f854acde71dd837b08603890602a4a7ac7
Prasanna gramopadhye <69106022+gramo37@users.noreply.github.com>
2022-12-21 12:49:56 +0530

    Promise Sort Assignment Solution
    Create a file sorted.txt will all the numbers from 1.txt, 2.txt and 3.txt

--- javascript/04/async-assignment/multisort.js
+++ javascript/04/async-assignment/multisort.js
@@ -11,3 +11,35 @@ const fs = require('fs')
  * 
  * NOTE: You can NOT use readFileSync or writeFileSync 
  */
+
+const fileNames = ['/1.txt', '/2.txt', '/3.txt']
+const arrOfPromises = []
+
+function mergeAllIntoOne(arrs) {
+    let ans = [];
+    arrs.forEach(element => {
+        ans = [...ans, ...element]
+    });
+    ans.sort((a, b) => a - b);
+    return (ans);
+}
+
+fileNames.forEach(filename => {
+    let promise = new Promise((resolve, reject) => {
+        fs.readFile(__dirname + filename, function (err, data) {
+            if (err) throw err;
+            let array = data.toString().split("\n");
+            var numberArray = array.map(Number);
+            numberArray.sort((a, b) => a - b)
+            resolve(numberArray);
+        })
+    })
+    arrOfPromises.push(promise);
+})
+
+Promise.all(arrOfPromises).then((arrs) => {
+    var file = fs.createWriteStream(__dirname + '/sorted.txt');
+    file.on('error', function (err) { throw err });
+    mergeAllIntoOne(arrs).forEach(function (v) { file.write(v + '\n'); });
+    file.end();
+})
